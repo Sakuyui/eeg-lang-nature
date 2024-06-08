70c7ae77ca42bfcf06327a13750de3822632ee8e
Huang Wanhong <405544641@qq.com>
2024-06-08 23:48:58 +0900

    add configuration accesso

--- scripts/experiment-main.py
+++ scripts/experiment-main.py
@@ -16,12 +16,55 @@ class Experiment(object):
     def __init__(self, configuration: Dict):
         self.configuration = configuration
     
+    
+    def is_configuration_exist(self, configure_name, recursively = False) -> bool:
+        def recursively_check(dictionary):
+            if dictionary == None or not isinstance(dictionary, dict):
+                return False
+            if configure_name in dictionary:
+                return True
+            for key in dictionary.keys():
+                check_result = recursively_check(dictionary[key])
+                if check_result:
+                    return True
+            return False
+            
+        if configure_name in self.configuration:
+            return True
+        if not recursively:
+            return False
+        return recursively_check(self.configuration)
+    
+    def get_bool_congiguration_item(self, item_name, recursively = False):
+        result = self.get_configuration_item_if_exist(item_name, recursively)
+        if result == None or not isinstance(result, bool):
+            return False
+        return result
+    
+    def get_configuration_item_if_exist(self, item_name, recursively = False):
+        def recursively_get(dictionary):
+            if dictionary == None or not isinstance(dictionary, dict):
+                return None
+            if item_name in dictionary:
+                return dictionary[item_name]
+            for key in dictionary.keys():
+                result = recursively_get(dictionary[key])
+                if result != None:
+                    return result
+            return None
+            
+        if item_name in self.configuration:
+            return self.configuration[item_name]
+        if not recursively:
+            return None
+        return recursively_get(self.configuration)
+        
     def load_eeg_data(self):
-        raw_file_path = self.configuration['raw_file_path']
+        raw_file_path = self.get_configure_item_if_exist('raw_file_path')
         print("Load raw file from %s" % raw_file_path)
         raw_data = mne.io.read_raw(raw_file_path)
         info = raw_data.info
-        if "ch_names" in self.configuration:
+        if self.is_configuration_exist("ch_names"):
             mne.rename_channels(info, {x:y for x, y in zip(info.ch_names, self.configuration['ch_names'])})
         montage = mne.channels.make_standard_montage(self.configuration['montage'])
         raw_data.set_montage(montage)
@@ -47,7 +90,8 @@ class Experiment(object):
     def word_list_building(self, eeg_matrix, eeg_info) -> AbstractEEGWordList:
         word_list_builder: AbstractWordListBuilder = self.select_word_list_builder()
         word_list = None
-        if 'build_word_list_from_file' in self.configuration and self.configuration['build_word_list_from_file']:
+        
+        if self.get_bool_congiguration_item('build_word_list_from_file'):
             word_list = word_list_builder.deserialize_from_file(self.configuration['input_word_list_file_path'])
             print(" ------ deserialize word list from %s ------" % self.configuration['input_word_list_file_path'])
             return word_list
@@ -92,7 +136,7 @@ class Experiment(object):
         eeg_info = self.get_eeg_info(eeg_data)
         eeg_matrix = self.preprocess(eeg_matrix)
         word_list: AbstractEEGWordList = self.word_list_building(eeg_matrix, eeg_info)
-        if 'serialize_word_list' in self.configuration and self.configuration['serialize_word_list']:
+        if self.get_bool_congiguration_item('serialize_word_list'):
             print('  ------ Serialize Word List to %s ------' % self.configuration['output_word_list_file_path'])
             word_list.serialize_to(self.configuration['output_word_list_file_path'])
         
@@ -101,11 +145,16 @@ class Experiment(object):
             dataset_for_segmentation = eeg_matrix
         
         print("  ------ Segmenting ------")
-        word_sequence = self.select_lexer().segment(word_list, dataset_for_segmentation, self.electrode_location_map)
+        word_sequence = None
+        if self.get_bool_congiguration_item("load_word_sequence_from_file"):
+            word_sequence = np.load(self.configuration['input_word_sequence_file_path'])
+        else:
+            word_sequence = self.select_lexer().segment(word_list, dataset_for_segmentation, self.electrode_location_map)
+        
         print(word_sequence)
-        if 'save_word_sequence' in self.configuration and self.configuration['save_word_sequence']:
-            print('  ------ Save Word Sequence to %s ------' % self.configuration['output_word_list_file_path'])
-            np.save(self.configuration['word_sequence_file_path'], word_sequence)
+        if not self.get_bool_congiguration_item('build_word_list_from_file') and self.get_bool_congiguration_item('save_word_sequence'):
+            print('  ------ Save Segmentation Results (Word Sequence) to %s ------' % self.configuration['output_word_sequence_file_path'])
+            np.save(self.configuration['output_word_sequence_file_path'], word_sequence)
         print("-------- End Building EEG Language --------")
 
     def evaluation_experiment(self):
@@ -151,7 +200,10 @@ experiment_configuration1 = {
     'input_word_list_file_path': 'C:/Users/Micro/Desktop/Research/eeg-language/data/word_list.wl.npy',
     'lexer': 'gfp-electrode-value-based-lexer',
     'save_word_sequence': True,
-    'word_sequence_file_path': 'C:/Users/Micro/Desktop/Research/eeg-language/data/word_sequence.ws.npy'
+    'load_word_sequence_from_file': True,
+    'input_word_sequence_file_path': 'C:/Users/Micro/Desktop/Research/eeg-language/data/word_sequence.ws.npy',
+    'output_word_sequence_file_path': 'C:/Users/Micro/Desktop/Research/eeg-language/data/word_sequence.ws.npy',
+
 }
 
 experiment = Experiment(experiment_configuration1)
